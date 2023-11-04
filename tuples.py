82c69366e9a03e8762a9cd22db4befc892b1db43
mithundesai <102952285+mithundesai@users.noreply.github.com>
2023-11-04 18:06:16 +0530

    first commit

---
+++ tuples.py
@@ -0,0 +1,41 @@
+list = [1, 2, 3]
+
+tuple = (1, "two", "3")
+
+print(tuple[0])
+print(tuple[1])
+
+x, y, z = tuple
+
+print(x)
+print(y)
+print(z)
+
+student = {
+    "name": "ram",
+    "age": 15,
+    "address": "pune",
+    "subjects": ["maths", "English"],
+    "parents": {
+        "mom": "geeta",
+        "dad": "sham"
+    }
+}
+
+print(student)
+print(student["name"])
+print(student["age"])
+# student["country"] = "India"
+print(student["parents"])
+# print(student)
+print(student.get("name"))
+print(student.get("course", "default"))
+
+Dic
+
+employee :
+
+name
+salary
+departemts
+
