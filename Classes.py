dbe20b1e3f27df1e11d76a61b7f8b2d82aad8852
mithundesai <102952285+mithundesai@users.noreply.github.com>
2023-11-23 11:05:07 +0530

    classes example

--- Classes.py
+++ Classes.py
@@ -16,7 +16,36 @@ p1.draw()
 p1.calulate()
 print(p1.x)
 
+
 # rectangle 2 properties length and breadth , methods area and perimeter
 
 # p2 = Point(20,90)
 # print(p2.x)
+
+
+class BankAccount:
+
+    def __init__(self, name, accountbalance):
+        self.name = name
+        self.accountBalance = accountbalance
+
+    def getName(self):
+        print(self.name)
+
+    def getAccountBalance(self):
+        print(self.accountBalance)
+
+    @staticmethod
+    def getRateofIntersent():
+        print(f" rate of interest is 10% ")
+
+
+BankAccount.getRateofIntersent()
+acoount1 = BankAccount("ram", 10000)
+
+acoount1.getAccountBalance()
+
+#BankAccount.getAccountBalance(acoount1)
+
+# student name roll no , address
+# employww name , departmet ,
