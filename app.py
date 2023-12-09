b054fecae7a403a0490e7f7b7d4233d801b36794
mithundesai <102952285+mithundesai@users.noreply.github.com>
2023-12-09 13:57:30 +0530

    modules and packages

---
+++ app.py
@@ -0,0 +1,7 @@
+import Ploymorphisem
+import Ploymorphisem as p
+from Ploymorphisem import method1
+from utils.modules import do_some
+
+method1("test","")
+do_some()
---
+++ utils/modules.py
@@ -0,0 +1,2 @@
+def do_some():
+    print("abc")
